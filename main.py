b8a416b24c61c3c8c5b963f95bae12bd229d15c9
danyguancha <dany.1701627413@ucaldas.edu.co>
2024-03-15 21:41:40 -0500

    Modificaciones tabla

--- main.py
+++ main.py
@@ -24,9 +24,9 @@ def cargarArchivo(file):
         for nodeData in grafo["graph"][0]["data"]:
             node_id = nodeData["id"]
             nodes.append(Node(id=node_id, size=nodeData["radius"], 
-                            #label=nodeData["label"], 
-                            label='👾', 
-                            type=nodeData["type"], data=nodeData["data"], color="green", shape=None))
+                            label=nodeData["label"], 
+                            #label='👾', 
+                            type=nodeData["type"], data=nodeData["data"], color="green"))
 
         for nodeData in grafo["graph"][0]["data"]:
             node_id = nodeData["id"]
@@ -39,18 +39,18 @@ def cargarArchivo(file):
                                 width=3, color=edge_color, directed=False))
                 if not any(node.id == linked_node_id for node in nodes):
                     nodes.append(Node(id=linked_node_id, size=20, 
-                                      #label=str(linked_node_id), 
-                                      label='👾',
-                                      type="circle", color="blue", shape=None))
+                                      label=str(linked_node_id), 
+                                      #label='👾',
+                                      type="circle", color="blue"))
             
     else:
         dirigido = grafo["directed"]
         for nodeData in grafo["nodes"]:
             node_id = nodeData["id"]
             nodes.append(Node(id=node_id, title=nodeData["title"],
-                              #label=nodeData["label"],
-                              label='👾',
-                              shape=None,size=nodeData["size"],color=nodeData["color"]))
+                              label=nodeData["label"],
+                              #label='👾',
+                              size=nodeData["size"],color=nodeData["color"]))
             
         for edgeData in grafo["edges"]:
             source_node_id = edgeData["from"]
@@ -59,7 +59,6 @@ def cargarArchivo(file):
             edges.append(Edge(source=source_node_id, target=target_node_id, label=str(edgeData["label"]), 
                             width=3, color=edgeData["color"], directed=edgeData["directed"]))
     return nodes, edges, dirigido
-
 
 def cargarGrafo():
     file = st.file_uploader("Cargar archivo JSON", type=["json"])
@@ -72,6 +71,7 @@ def main():
     logArista = LogArista()
     logGrafo = LogGrafo()
     bandera = False
+    estado = False
     with st.sidebar:
         st.session_state.directed = None
         selected = option_menu(
@@ -105,6 +105,8 @@ def main():
                         st.session_state.nodes = nodes
                         st.session_state.edges = edges
                         bandera = True
+                        estado = True
+                        
                         
                     elif selectTipo == "Completo":
                         nuevaOp = st.selectbox("Seleccione el tipo de grafo", [" ",
@@ -117,12 +119,17 @@ def main():
                                 st.session_state.nodes = nodes
                                 st.session_state.edges = edges
                                 st.session_state.directed = True
+ 
                             if st.session_state.directed:
                                 bandera = True
+                                estado = True
                         elif nuevaOp == "Grafo no dirigido":
-                            nodes, edges = logGrafo.generarGrafoCompleto(num_nodes, selectTipo, Node, Edge)
-                            st.session_state.nodes = nodes
-                            st.session_state.edges = edges
+                            if st.session_state.directed == None:
+                                nodes, edges = logGrafo.generarGrafoCompleto(num_nodes, selectTipo, Node, Edge)
+                                st.session_state.nodes = nodes
+                                st.session_state.edges = edges
+                                bandera = False
+                            estado = True
                                 
                     
                        
@@ -130,6 +137,7 @@ def main():
                 
                 if st.session_state.directed == None:
                     cargarGrafo()
+                    estado = True
                 if st.session_state.directed:
                     bandera = True
                 
@@ -138,8 +146,7 @@ def main():
                 st.session_state.edges = []
                 st.session_state.grafo_cargado = False
             elif selected_option == "Exportar Datos":
-                if logGrafo.esCompleto(len(st.session_state.nodes), len(st.session_state.edges)):
-                    bandera = True
+                
                 selected_sub_option = st.selectbox(
                     "Formato a exportar:",
                     [" ","JSON", "CSV", "Excel", "Imagen"]
@@ -149,6 +156,9 @@ def main():
                     nombreArchivo = 'grafo_exportado.json'
                     nombreCompleto = ruta + nombreArchivo
                     logGrafo.exportarGrafoJson(nombreCompleto, st.session_state.nodes, st.session_state.edges,Node, st)
+                elif selected_sub_option == "CSV":
+                    estado = True
+                    
                 elif selected_sub_option == "Excel":
                     logGrafo.exportarGrafoExcel('excel',st.session_state.nodes, st.session_state.edges, st)
                 elif selected_sub_option == "Imagen":
@@ -166,8 +176,10 @@ def main():
             elif selected_option == "Nodo":
                 st.sidebar.header("Nodos")
                 logNodo.agregarNodo(Node, st)
+                logNodo.cambiarEtiquetaNodo(st)
                 logNodo.cambiarColorNodo(st)
                 logNodo.eliminarNodo(st)
+                
                     
             #=======================Seccion de arcos=======================
             elif selected_option == "Arista":
@@ -200,11 +212,16 @@ def main():
                 "Seleccionar opción:",
                 ["Ayuda", "Acerca de Grafos"]
             )
+            if selected_option == "Ayuda":
+                ayuda = True
             
     
         
     # Navbar
     st.title("Proyecto de Análisis de Algoritmos")
+    #if ayuda:
+        #LectorArchivo().leerArchivoPdf('E:\proyectosU\semestres\semestre-2024-1\Analisis\proyectoAnalisis\Data\ManualUsuario.pdf')
+
     if "nodes" not in st.session_state:
         st.warning("No se ha cargado ningún archivo.")
     else:
@@ -213,9 +230,13 @@ def main():
             with st.container(border=True):
                 agraph(nodes=st.session_state.nodes, edges=st.session_state.edges, config=Gui(True))
         else:
+          
             with st.container(border=True):
                 config = Gui(False)
                 agraph(nodes=st.session_state.nodes, edges=st.session_state.edges, config=config)
+        if estado:
+            logGrafo.mostrarDatosGrafoTabla(st.session_state.nodes, st.session_state.edges,st)
+        
 
 if __name__ == "__main__":
     main()
